666864f143ec7dd64d53037726fc92dab3fc13a8
Fabiano Fregnani <99106631+fregnani123@users.noreply.github.com>
2024-05-28 16:42:50 -0300

    Update interface.js

--- interface.js
+++ interface.js
@@ -1,24 +1,25 @@
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('DOMContentLoaded', () => {
 
     let squares = document.querySelectorAll(".square");
 
-    squares.forEach((square)=>{
-         square.addEventListener ('click', handleClick);
-            
-   })
-   
+    squares.forEach((square) => {
+        square.addEventListener('click', handleClick);
+    })
+
+    // Adiciona um evento para o botão limpar
+    let limparButton = document.getElementById('limpar');
+    limparButton.addEventListener('click', handleClearBoard);
 })
 
 function handleClick(event) {
     let square = event.target;
-    let postion = square.id;
+    let position = square.id;
 
-   if  (handleMove(postion)){
-
-   setTimeout(() =>{
-        alert('O jogo acabou,o Player ' +  playerTime);
-   }, 10); 
-};
+    if (handleMove(position)) {
+        setTimeout(() => {
+            alert('O jogo acabou, o Player ' + playerTime);
+        }, 10);
+    }
 
     updateSquares();
 }
@@ -28,11 +29,18 @@ function updateSquares() {
 
     squares.forEach((square) => {
         let position = square.id;
-        let symbols = board[position];
+        let symbol = board[position];
 
-  if ( symbols != '')
-  square.innerHTML = `<div class='${symbols}'></div>`
-})
+        square.innerHTML = symbol ? `<div class='${symbol}'></div>` : '';
+    })
+}
 
+function handleClearBoard() {
+    // Redefine o tabuleiro
+    board = ['', '', '', '', '', '', '', '', ''];
+    playerTime = 0;
+    gameOver = false;
 
-} 
+    // Atualiza a interface gráfica
+    updateSquares();
+}
